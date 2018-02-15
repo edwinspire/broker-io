730c5ac51fb2080ae2dd036f7d892d249cb8bcaf
edwinspire <edwinspire@gmail.com>
2018-02-15 08:13:00 -0500

    Se logra controlar los canales desde Postgresql

--- test/client.js
+++ test/client.js
@@ -2,13 +2,13 @@ const io = require('socket.io-client');
 const dojo = require('@dojo/core');
 
 
-const ioc = io("http://localhost:3000/log");
+const ioc = io("http://localhost:3000/public/test");
 
 
 console.log(dojo.DateObject.now().toString());
 
-ioc.on('devuelve', function(socket){
+ioc.on('log', function(socket){
   console.log('a user connected', socket);
-  ioc.emit("rec", "Recibido");
+  ioc.emit("log", {inicio: dojo.DateObject.now(), fin: 'Estamos probando'});
 });
 
